09497e5ca5dc08182b018a4db2c57340998c379b
sweetzxom <sweetzxom@gmail.com>
2026-01-14 13:55:34 +0800

    Add files via upload

---
+++ lavalink.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+      name: "GlceYT",
+      password: "glace",
+      host: "de-01.strixnodes.com",
+      port: 2010,
+      secure: false
+    }
+};           
+
+
